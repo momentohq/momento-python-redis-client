2320f56e2d77bcc84b936caf5bf9e9fb4dd408d9
Pete Gautier <pete@momentohq.com>
2023-08-21 17:01:07 -0700

    fix: fix incorrect environment variable name

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,7 +15,7 @@ def momento_redis_client():  # type: ignore
     cache_name = f"momento-python-redis-client-test-{uuid.uuid4()}"
     with momento.CacheClient(
         momento.Configurations.Laptop.latest(),
-        momento.CredentialProvider.from_environment_variable("MOMENTO_AUTH_TOKEN"),
+        momento.CredentialProvider.from_environment_variable("TEST_AUTH_TOKEN"),
         timedelta(seconds=60),
     ) as client:
         client.create_cache(cache_name)
